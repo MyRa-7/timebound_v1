f5ad09e4d6d3617d472a7015a12a87a817f4a3f8
MyRa-7 <MyRa-7@users.noreply.github.com>
2017-07-02 23:14:42 +0530

    Adding Authorisation for ensuring not everyone can edit all objects

--- timebound_v1/views.py
+++ timebound_v1/views.py
@@ -2,6 +2,10 @@ from django.shortcuts import render, redirect
 
 from timebound_v1.forms import ThingForm
 from timebound_v1.models import Thing
+from django.template.defaultfilters import slugify
+
+from django.contrib.auth.decorators import login_required
+from django.http import Http404
 
 # Create your views here.
 def index(request):
@@ -18,9 +22,13 @@ def thing_detail(request, slug):
 			'thing': thing,
 		})
 
+@login_required
 def edit_thing(request, slug):
 	thing = Thing.objects.get(slug=slug)
 	form_class = ThingForm
+
+	if thing.user != request.user:
+		raise Http404
 
 	if request.method == 'POST':
 		form = form_class(data=request.POST, instance=thing)
@@ -33,6 +41,28 @@ def edit_thing(request, slug):
 	return render(request, 'things/edit_thing.html', {
 			'thing': thing,
 			'form' : form,
+		})
+
+def create_thing(request):
+	form_class = ThingForm
+
+	if request.method == 'POST':
+		form = form_class(request.POST)
+		if form.is_valid():
+			thing = form.save(commit=False)
+
+			thing.user = request.user
+			thing.slug = slugify(thing.name)
+
+			thing.save()
+
+			return redirect('thing_detail', slug=thing.slug)
+
+	else:
+		form = form_class()
+
+	return render(request, 'things/create_thing.html', {
+			'form': form,
 		})
 
 
